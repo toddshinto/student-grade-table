88dc2bfa18b6a335ce7c0db4f1fd1e2261af0b27
Todd Shinto <todd_thirty_one@yahoo.com>
2020-05-07 13:01:07 -0700

    added gradeform component and functionality

---
+++ components/grade-form.js
@@ -0,0 +1,14 @@
+class GradeForm {
+  constructor(formElement) {
+    this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener('submit', this.handleSubmit);
+  }
+  onSubmit(createGrade) {
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('wow nice');
+  }
+}
--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var newGradeTable = new GradeTable(tableElement);
 
 var header = document.querySelector('header');
 var pageHeader = new PageHeader(header);
+var form = document.querySelector('form');
 
-var app = new App(newGradeTable, pageHeader);
+var gradeForm = new GradeForm(form);
+
+var app = new App(newGradeTable, pageHeader, gradeForm);
 app.start();
